703e60e92a1360ceafbd3eac9b04f089b11e4ba1
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-03-29 00:04:34 +0530

    .

---
+++ 5_typeCasting.py
@@ -0,0 +1,13 @@
+a = "1" 
+b = "2"
+print(a+b) # answer will be '12' bcz 'a' and 'b' are both strings
+
+# Typecasting : converting one data type to another datatype
+print(int(a) + int(b)) # this is Explicit Typecasting
+
+# if a = "vishal" it cannot be typecasted into Integer bcz its not a valid integer
+
+# Implicit Typecasting
+c = 1.2
+d = 3
+print(c+d) # answer will be of FLOAT type
---
+++ 6_input.py
@@ -0,0 +1,4 @@
+a = input("Enter value of a : ")
+b = input("Enter value of b : ")
+
+print("Sum of",a, "and",b, "is : ", int(a)+int(b))
