be224bfeb59c2d869aca517b1f65dadb86790b84
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-03-28 23:18:41 +0530

    .

---
+++ 4_calculator.py
@@ -0,0 +1,17 @@
+a = 5
+b = 4
+
+# Addition
+print("The Value of",a, "+" ,b , "=",a+b) # 9
+# Subtraction
+print("The Value of",a, "-" ,b , "=",a-b) # 1
+# Multiplication
+print("The Value of",a, "*" ,b , "=",a*b) # 20
+# Division
+print("The Value of",a, "/" ,b , "=",a/b) # 1.25
+# Exponentiation
+print("The Value of",a, "**" ,b , "=",a**b) # 5^4
+# Float Division
+print("The Value of",a, "/" ,b , "=",a/b) # 1 (no decimal value)
+# Modulo
+print("The Value of",a, "%" ,b , "=",a%b) # 1
