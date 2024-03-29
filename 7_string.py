6a79e43a3b5637b16e0227222bccd9a5447efe28
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-03-29 12:33:27 +0530

    .

---
+++ 7_string.py
@@ -0,0 +1,16 @@
+a = "Hello Vishal" # Double Quote
+b = 'How are You?' # Single Quote
+
+# Multiline String using Triple Quote
+c = ''' I am Fine
+what about you?
+Haven't seen you for a while
+where were you?
+'''
+# print(a)
+# print(b)
+# print(c)
+
+# Print all the characters of the String
+for character in c:
+    print(character)
